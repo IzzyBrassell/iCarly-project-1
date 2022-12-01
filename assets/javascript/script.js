70051173d91dd5f86f76349fa14d3939ab054313
Israel (Izzy) Brassell <israel.brassell@gmail.com>
2022-12-01 18:47:17 -0500

    added video to .js file

---
+++ assets/javascript/script.js
@@ -0,0 +1,114 @@
+// var tag = document.createElement('script')
+// tag.src = "https://youtube.com/iframe_api"
+// var firstScriptTag = document.getElementsByTagName('script')[0];
+//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+if (document.documentURI === 'https://izzybrassell.github.io/iCarly-project-1/') {
+    var tag = document.createElement('script')
+tag.src = "https://youtube.com/iframe_api"
+var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: '854jxWy5L5Y',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+   
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+        //   setTimeout(stopVideo, 6000);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+} else if (document.documentURI === 'https://izzybrassell.github.io/iCarly-project-1/This.html') {
+    var tag = document.createElement('script')
+tag.src = "https://youtube.com/iframe_api"
+var firstScriptTag = document.getElementsByTagName('script')[0];
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'eOh-E_DvyYk',
+          playerVars: {
+            'playsinline': 1
+          },
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }
+
+   
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
+
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+        //   setTimeout(stopVideo, 0);
+          done = true;
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+} else {
+    var tag = document.createElement('script')
+    tag.src = "https://youtube.com/iframe_api"
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+          firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        var player;
+          function onYouTubeIframeAPIReady() {
+            player = new YT.Player('player', {
+              height: '390',
+              width: '640',
+              videoId: 'S6FeBBhu83A',
+              playerVars: {
+                'playsinline': 1
+              },
+              events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+              }
+            });
+          }
+    
+       
+          function onPlayerReady(event) {
+            event.target.playVideo();
+          }
+    
+          var done = false;
+          function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING && !done) {
+            //   setTimeout(stopVideo, 0);
+              done = true;
+            }
+          }
+          function stopVideo() {
+            player.stopVideo();
+          }
+}
