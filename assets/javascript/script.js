75b6332ea8ffaed8dc07201080f32bb3d0f5e6de
Israel (Izzy) Brassell <israel.brassell@gmail.com>
2022-12-10 19:54:23 -0500

    Fixed bugged Framework

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -37,6 +37,57 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
       function stopVideo() {
         player.stopVideo();
       }
+      function loadClient() {
+        gapi.client.setApiKey("AIzaSyBrZq3gWBbOv5Rsns1HgkUdhZUcP91HDJk");
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+            .then(function() { 
+              console.log("GAPI client loaded for API"); 
+              execute()
+          },
+                  function(err) { console.error("Error loading GAPI client for API", err); });
+      }
+      // Make sure the client is loaded before calling this method.
+      function execute() {
+        return gapi.client.youtube.commentThreads.list({
+          "part": [
+            "id, snippet"
+          ],
+          "videoId": "S9NMm75c832k"
+        })
+            .then(function(response) {
+              var commentarray = response.result.items
+              for(let i=0; i < commentarray.length ;i++) {
+                comments = document.createElement(`div`)
+                authorName = document.createElement(`div`)
+                document.setAttribute(``)
+                commentsection = document.getElementById("commentsection")
+                comments.innerHTML+= commentarray[i].snippet.topLevelComment.snippet.textOriginal
+                authorName.innerHTML+= commentarray[i].snippet.topLevelComment.snippet.authorDisplayName
+                commentsection.append(authorName)
+                commentsection.append(comments)
+                
+                var authorAvatar = commentarray[i].snippet.topLevelComment.snippet.authorProfileImageUrl
+    
+                console.log(comments)
+                
+                // document.getElementById("commentsection").innerHTML+=authorName 
+                
+    
+                // document.getElementById("commentsection").innerHTML+=comments
+                
+                
+                
+                // document.getElementById("commentsection").innerHTML+=authorAvatar
+    
+    
+        
+              }
+                    // Handle the results here (response.result has the parsed body) (JSON).
+                    console.log("Response", response);
+                  },
+                  
+                  function(err) { console.error("Execute error", err); });
+      }
 } else if (document.documentURI === 'https://izzybrassell.github.io/iCarly-project-1/This.html') {
     var tag = document.createElement('script')
 tag.src = "https://youtube.com/iframe_api"
@@ -72,6 +123,57 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
       }
       function stopVideo() {
         player.stopVideo();
+      }
+      function loadClient() {
+        gapi.client.setApiKey("AIzaSyBrZq3gWBbOv5Rsns1HgkUdhZUcP91HDJk");
+        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+            .then(function() { 
+              console.log("GAPI client loaded for API"); 
+              execute()
+          },
+                  function(err) { console.error("Error loading GAPI client for API", err); });
+      }
+      // Make sure the client is loaded before calling this method.
+      function execute() {
+        return gapi.client.youtube.commentThreads.list({
+          "part": [
+            "id, snippet"
+          ],
+          "videoId": "eOh-E_DvyYk"
+        })
+            .then(function(response) {
+              var commentarray = response.result.items
+              for(let i=0; i < commentarray.length ;i++) {
+                comments = document.createElement(`div`)
+                authorName = document.createElement(`div`)
+                document.setAttribute(``)
+                commentsection = document.getElementById("commentsection")
+                comments.innerHTML+= commentarray[i].snippet.topLevelComment.snippet.textOriginal
+                authorName.innerHTML+= commentarray[i].snippet.topLevelComment.snippet.authorDisplayName
+                commentsection.append(authorName)
+                commentsection.append(comments)
+                
+                var authorAvatar = commentarray[i].snippet.topLevelComment.snippet.authorProfileImageUrl
+    
+                console.log(comments)
+                
+                // document.getElementById("commentsection").innerHTML+=authorName 
+                
+    
+                // document.getElementById("commentsection").innerHTML+=comments
+                
+                
+                
+                // document.getElementById("commentsection").innerHTML+=authorAvatar
+    
+    
+        
+              }
+                    // Handle the results here (response.result has the parsed body) (JSON).
+                    console.log("Response", response);
+                  },
+                  
+                  function(err) { console.error("Execute error", err); });
       }
 } else {
     var tag = document.createElement('script')
